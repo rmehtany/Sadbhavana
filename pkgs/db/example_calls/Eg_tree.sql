6eab37790c11c02e75fd6a77e346cfc42a590291
Sandip Mehta <sandip.mehta@omnicomgrp.com>
2025-11-27 22:37:07 -0500

    Upload added P_GetProjectTreeCnts

--- pkgs/db/example_calls/Eg_tree.sql
+++ pkgs/db/example_calls/Eg_tree.sql
@@ -2,6 +2,25 @@ DO $RUN$
 DECLARE
     v_output JSONB;
 BEGIN
+    CALL P_GetProjectTreeCnts (
+    '{
+		  "east_lng": 84.5947265625,
+		  "west_lng": 63.50097656250001,
+		  "north_lat": 25.423431426334247,
+		  "south_lat": 16.0774858690887
+    }'::jsonb,
+    v_output
+    );
+
+    -- Display the output
+    RAISE NOTICE 'Output: %', v_output;
+END 
+$RUN$;
+
+DO $RUN$
+DECLARE
+    v_output JSONB;
+BEGIN
     CALL P_GetTreeClusters (
     '{
 		  "zoom": 7,
--- pkgs/db/procedures/SP_tree.sql
+++ pkgs/db/procedures/SP_tree.sql
@@ -1,4 +1,63 @@
 -- Create the stored procedure with transaction management
+CREATE OR REPLACE PROCEDURE P_GetProjectTreeCnts(
+    IN p_input_json JSONB,
+    INOUT p_output_json JSONB DEFAULT NULL
+)
+LANGUAGE plpgsql
+AS $BODY$
+DECLARE
+	v_rows_affected INTEGER;
+	v_east_lng FLOAT8;
+	v_west_lng FLOAT8;
+	v_north_lat FLOAT8;
+	v_south_lat FLOAT8;
+BEGIN
+	v_east_lng = p_input_json->>'east_lng';
+	v_west_lng = p_input_json->>'west_lng';
+	v_north_lat = p_input_json->>'north_lat';
+	v_south_lat = p_input_json->>'south_lat';
+
+	SELECT jsonb_agg(
+	    jsonb_build_object(
+	        'project_code', project_code,
+	        'project_name', project_name,
+	        'tree_count', tree_count,
+	        'center_lat', center_lat,
+	        'center_lng', center_lng
+	    )
+	)
+	INTO p_output_json
+	FROM 
+		(SELECT 
+		    t.project_code,
+		    min(tw.project_name) AS project_name,
+		    COUNT(*) as tree_count,
+		    AVG(ST_Y(t.tree_location::geometry))::FLOAT as center_lat,
+		    AVG(ST_X(t.tree_location::geometry))::FLOAT as center_lng
+		FROM core.tree t
+			JOIN core.project tw 
+				ON t.project_code=tw.project_code
+		WHERE ST_Y(t.tree_location::geometry) BETWEEN v_south_lat AND v_north_lat
+		AND ST_X(t.tree_location::geometry) BETWEEN v_west_lng AND v_east_lng
+		GROUP BY t.project_code
+		);
+	
+    -- Return empty array if no output
+--    IF p_output_json IS NULL THEN
+--        p_output_json := '[]'::jsonb;
+--    END IF;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        -- Rollback on any error
+        ROLLBACK;
+        
+        -- Re-raise the exception with details
+        RAISE EXCEPTION 'Transaction failed: % (SQLSTATE: %)', SQLERRM, SQLSTATE;
+END;
+$BODY$;
+
+-- Create the stored procedure with transaction management
 CREATE OR REPLACE PROCEDURE P_GetTreeClusters(
     IN p_input_json JSONB,
     INOUT p_output_json JSONB DEFAULT NULL
@@ -21,17 +80,6 @@ BEGIN
 	v_north_lat = p_input_json->>'north_lat';
 	v_south_lat = p_input_json->>'south_lat';
 
-	WITH aggregated_trees AS (
-	    SELECT 
-	        ST_Y(ST_SnapToGrid(t.tree_location::geometry, v_gridSize, v_gridSize))::FLOAT AS grid_lat,
-	        ST_X(ST_SnapToGrid(t.tree_location::geometry, v_gridSize, v_gridSize))::FLOAT AS grid_lng,
-	        COUNT(*) AS tree_count,
-	        ARRAY_AGG(t.id)::VARCHAR[] AS tree_ids
-	    FROM core.tree t
-	    WHERE t.tree_location && ST_MakeEnvelope(v_west_lng, v_south_lat, v_east_lng, v_north_lat, 4326)::geography
-	    GROUP BY ST_SnapToGrid(t.tree_location::geometry, v_gridSize, v_gridSize)
-	    HAVING COUNT(*) > 0
-	)
 	SELECT jsonb_agg(
 	    jsonb_build_object(
 	        'grid_lat', grid_lat,
@@ -41,15 +89,18 @@ BEGIN
 	    )
 	)
 	INTO p_output_json
-	FROM aggregated_trees;
-	raise notice 'Hello';
-
-    -- Return empty array if no output
-    IF p_output_json IS NULL THEN
-        p_output_json := '[]'::jsonb;
-    END IF;
-    
-
+	FROM 
+		(SELECT 
+	        ST_Y(ST_SnapToGrid(t.tree_location::geometry, v_gridSize, v_gridSize))::FLOAT AS grid_lat,
+	        ST_X(ST_SnapToGrid(t.tree_location::geometry, v_gridSize, v_gridSize))::FLOAT AS grid_lng,
+	        COUNT(*) AS tree_count,
+	        ARRAY_AGG(t.id)::VARCHAR[] AS tree_ids
+	    FROM core.tree t
+	    WHERE t.tree_location && ST_MakeEnvelope(v_west_lng,v_south_lat,v_east_lng,v_north_lat, 4326)::geography
+	    GROUP BY ST_SnapToGrid(t.tree_location::geometry, v_gridSize, v_gridSize)
+	    HAVING COUNT(*)>0
+		);
+	
 EXCEPTION
     WHEN OTHERS THEN
         -- Rollback on any error
